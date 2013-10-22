094d200e9dff0ed5a28413fd5a466a96db0ef33b
Josh Lamb <joshlamb@gmail.com>
2013-10-22 14:17:12 -0400

    Exercise 4 complete (would like to improve)

--- exercise4.rb
+++ exercise4.rb
@@ -1,11 +1,13 @@
+# Iterates through 100 integers, printing Fizz for multiples of 3 and Buzz for multiples of 5
 (1..100).each do |x|
 	case 
+	when x % 3 == 0 && x % 5 == 0
+		puts "FizzBuzz"
 	when x % 3 == 0
-		print "Fizz"
+		puts "Fizz"
 	when x % 5 == 0
-		print "Buzz"
+		puts "Buzz"
 	else
-		print x
+		puts x
 	end
-	puts
 end
